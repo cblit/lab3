39c386a9c53346e6cb549940ec2a3841a6b689a6
chen bin <CHENB.22@ichat.sp.edu.sg>
2023-11-04 16:41:37 +0800

    lab3 excercise 4 part d

--- employee_info.py
+++ employee_info.py
@@ -30,10 +30,11 @@ def calculate_average_salary():
 
 def get_employees_by_dept(department):
     result = []
-
-    # Add your implementation from here
-
-
+    for item in employee_data:
+        if item["department"] == department:
+            print("testing")
+            result.append(item)
+            
     return result
 
 def display_all_records():
